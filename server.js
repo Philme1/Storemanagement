455c0a5be599c7baae30fd678afa4abb658560a3
Philme1 <philipinoo28@gmail.com>
2020-06-04 16:49:31 +0100

    modified .env file and db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV !== "production") {
-  require("dotenv").config({path: '.env' });
+  require("dotenv").config();
 }
+
 
 const express = require("express");
 const passport = require("passport");
